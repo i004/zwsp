2084d8e3cbcc738e2b965f3b5da1c57ea8cc3dea
u1o <artem129871@gmail.com>
2022-05-25 20:56:39 +0300

    Add README

--- README.md
+++ README.md
@@ -1,3 +1,43 @@
 # InvisibleData
 
-h
+Store data using zero width spaces! Might be useful for discord bots. 
+
+---
+### Usage
+Import the library:
+```typescript
+import { encode, decode} from 'invisible-data'; // typescript
+
+const { encode, decode } = require('invisible-data'); // node.js
+```
+Encoding:
+```typescript
+encode("Hello World!");
+encode(3.14159265359);
+encode(12345678987654321n);
+encode([ 123, "Hello", 321, "World" ]);
+encode({ a: 123, b: 321, c: "example", d: [ "Hello", "world" ] });
+```
+Decoding:
+```typescript
+decode(...);
+
+// will also keep the type:
+decode(encode("123")); // "123" (string)
+decode(encode(123)); // 123 (number)
+decode(encode([1, 2, 3])); // [1, 2, 3] (object)
+```
+---
+### Why?
+tbh, I don't really know. This might be useful for discord bots that don't use any database but still need to store certain information (for example, bots hosted on heroku).
+
+*That was just a random idea that came to my mind at 3 am lol.*
+
+![](https://i.imgur.com/YN3gjdM.png)
+
+It works because the characters this library uses are actually a zero-width spaces and they appear to be invisible. Users still can access the info you stored, but a regular user won't notice anything.
+
+---
+### Other examples
+
+Visit [examples](examples) directory for other examples (with discord.js)
---
+++ examples/suggestion-bot.ts
@@ -0,0 +1,46 @@
+import { Client, Intents, TextChannel } from 'discord.js';
+import { encode, decode } from 'invisible-data';
+
+const client = new Client({
+	intents: [ Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES ]
+});
+
+const SUGGESTION_CHANNEL_ID = '111111111111111111';
+
+client.on('messageCreate', async (message) => {
+	if (!message.guild || message.author.bot || !message.content)
+    	return;
+	const args = message.content.split(/\s+/);
+    
+    if (args[0] == '!suggest') {
+    	const data = { authorId: message.author.id };
+        const channel = client.channels.cache.get(SUGGESTION_CHANNEL_ID) as TextChannel;
+
+    	await channel.send({
+        	embeds: [{
+            	author: { name: message.author.username, iconURL: message.author.displayAvatarURL() },
+                description: args.slice(1).join(' ') + encode(data)
+            }]
+        })
+    	await message.reply('Your suggestion was successfully submitted!');
+    } else if (args[0] == '!edit-suggestion') {
+        const channel = client.channels.cache.get(SUGGESTION_CHANNEL_ID) as TextChannel;
+        const msg = await channel.messages.fetch(args[1]);
+        const data: { authorId: string } = decode(msg.embeds[0].description);
+
+        if (data.authorId != message.author.id)
+            await message.reply('This is not your suggestion!');
+        else {
+            await msg.edit({
+                embeds: [{
+                    author: { name: message.author.username, iconURL: message.author.displayAvatarURL() },
+                    description: args.slice(2).join(' ') + encode(data)
+                }]
+            });
+
+            await message.reply('Your suggestion was successfully edited!');
+        }
+    }
+});
+
+client.login('token');
