7d033a02c9ac70900537a1a5b75673065e74f2ef
u1o <artem129871@gmail.com>
2022-05-25 19:49:33 +0300

    Decoder (first prototype)

--- src/InvisibleData/decode.ts
+++ src/InvisibleData/decode.ts
@@ -1,4 +1,15 @@
-import { IDTypes, whitespaces, validator } from "./constants";
+import { IDTypes, whitespaces, validator, typeMap } from "./constants";
+
+function convertType (input: string, type: string): IDTypes {
+    if (type == typeMap.bigint)
+        return BigInt(input);
+    else if (type == typeMap.boolean || type == typeMap.object)
+        return JSON.parse(input);
+    else if (type == typeMap.number)
+        return parseFloat(input);
+    
+    return input.toString();
+}
 
 /**
  * 
@@ -6,6 +17,20 @@ import { IDTypes, whitespaces, validator } from "./constants";
  * @param {string} input Data to decode
  * @returns {IDTypes}
  */
-export function decode (input: string): IDTypes {
-    return '';
+export function decode (input: string): IDTypes | null {
+    const match = validator.exec(input);
+
+    if (!match) return null;
+
+    const [ type, encoded ] = [
+        input[0],
+        Array.from(input.slice(1))
+             .map(chr => whitespaces.indexOf(chr))
+             .join('')
+    ];
+
+    const chars = encoded.match(/.{1,4}/g);
+    const decoded = chars.map(char => String.fromCharCode(parseInt(char, whitespaces.length))).join('');
+    
+    return convertType(decoded, type);
 }
