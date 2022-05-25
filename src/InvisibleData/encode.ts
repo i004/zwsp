ae29b999da6b889a8de535f93a6a3b63f6812432
u1o <artem129871@gmail.com>
2022-05-25 19:22:24 +0300

    First encoder prototype

--- src/InvisibleData/encode.ts
+++ src/InvisibleData/encode.ts
@@ -1,11 +1,25 @@
 import { IDTypes, whitespaces } from "./constants";
 
+function parseType (input: IDTypes): [typeof input, string] {
+    if (typeof input == 'object')
+        return ['object', JSON.stringify(input)];
+
+    return [
+        typeof input,
+        input.toString()
+    ];
+}
+
 /**
  * 
- * Encode data
+ * Encodes data using zero width spaces
  * @param {IDTypes} input Data to encode
  * @returns {string}
  */
 export function encode (input: IDTypes): string {
-    return '';
+    const [ type, string ] = parseType(input);
+    const encodedType = { bigint: '\u180E', boolean: '\u200B', number: '\u200C', object: '\u200D', string: '\u2060' }[type.toString()] || '\u2060';
+    const encoded = Array.from(string).map(char => char.charCodeAt(0).toString(whitespaces.length).padStart(4, '0')).join('');
+    
+    return encodedType + Array.from(encoded).map(x => whitespaces[x]).join('');
 }
