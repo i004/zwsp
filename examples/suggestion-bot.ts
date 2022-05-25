b060bbbf20a789163896bedb4121d7e73b6e16b8
u1o <artem129871@gmail.com>
2022-05-25 21:42:40 +0300

    I hate adding comments 💀

--- examples/suggestion-bot.ts
+++ examples/suggestion-bot.ts
@@ -6,32 +6,36 @@ const client = new Client({
 });
 
 const SUGGESTION_CHANNEL_ID = '111111111111111111';
+type SuggestionData = { authorId: string };
 
 client.on('messageCreate', async (message) => {
-	if (!message.guild || message.author.bot || !message.content)
+	if (!message.guild || message.author.bot || !message.content) // ignore bots, DM messages and messages with no content
     	return;
-	const args = message.content.split(/\s+/);
+    
+	const args = message.content.split(' '); // split message by spaces
     
     if (args[0] == '!suggest') {
-    	const data = { authorId: message.author.id };
-        const channel = client.channels.cache.get(SUGGESTION_CHANNEL_ID) as TextChannel;
+    	const data: SuggestionData = { authorId: message.author.id }; // suggestion data
+        const suggestionChannel = client.channels.cache.get(SUGGESTION_CHANNEL_ID) as TextChannel;
 
-    	await channel.send({
+    	await suggestionChannel.send({
         	embeds: [{
             	author: { name: message.author.username, iconURL: message.author.displayAvatarURL() },
-                description: args.slice(1).join(' ') + encode(data)
+                description: args.slice(1).join(' ') + encode(data) // merge suggestion text with encoded suggestion data
             }]
         })
+
     	await message.reply('Your suggestion was successfully submitted!');
     } else if (args[0] == '!edit-suggestion') {
-        const channel = client.channels.cache.get(SUGGESTION_CHANNEL_ID) as TextChannel;
-        const msg = await channel.messages.fetch(args[1]);
-        const data: { authorId: string } = decode(msg.embeds[0].description);
+        const suggestionChannel = client.channels.cache.get(SUGGESTION_CHANNEL_ID) as TextChannel;
+        const suggestionMessage = await suggestionChannel.messages.fetch(args[1]); // fetch message
 
-        if (data.authorId != message.author.id)
+        const data: SuggestionData = decode(suggestionMessage.embeds[0].description); // extract data from embed description
+
+        if (data.authorId != message.author.id) // prohibit users from editing suggestions they didn't submit
             await message.reply('This is not your suggestion!');
         else {
-            await msg.edit({
+            await suggestionMessage.edit({
                 embeds: [{
                     author: { name: message.author.username, iconURL: message.author.displayAvatarURL() },
                     description: args.slice(2).join(' ') + encode(data)
@@ -43,4 +47,4 @@ client.on('messageCreate', async (message) => {
     }
 });
 
-client.login('dQw4w9WgXcQye24lkh9boahj.tfb_Ct.zp3cliAHhmPEMa31HsrldJOE91j8ke7ZKrT07I');
+client.login('dQw4w9WgXcQ11ablkhaboahj.tfb_Ct.zp3cliAHhmPEMa31HsrldJOE91j8ke7ZKrT07I');
