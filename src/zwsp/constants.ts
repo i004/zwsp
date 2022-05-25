8e0d63962b36ec9fbce7f122a3662285f40530ae
u1o <artem129871@gmail.com>
2022-05-25 21:16:15 +0300

    Remove mongolian vowel separator from whitespace list

--- src/zwsp/constants.ts
+++ src/zwsp/constants.ts
@@ -1,11 +1,11 @@
 export type IDTypes = bigint | boolean | number | object | string;
 
-export const whitespaces = ['\u180E', '\u200B', '\u200C', '\u200D', '\u2060', '\uFEFF'];
-export const validator = /[\u180E\u200B\u200C\u200D\u2060][\u180E\u200B\u200C\u200D\u2060\uFEFF]{4,}/;
+export const whitespaces = ['\u200B', '\u200C', '\u200D', '\u2060', '\uFEFF'];
+export const validator = /[\u200B\u200C\u200D\u2060\uFEFF][\u180E\u200B\u200C\u200D\u2060\uFEFF]{4,}/;
 export const typeMap = {
-    bigint: '\u180E',
-    boolean: '\u200B',
-    number: '\u200C',
-    object: '\u200D',
-    string: '\u2060'
+    bigint: '\u200B',
+    boolean: '\u200C',
+    number: '\u200D',
+    object: '\u2060',
+    string: '\uFEFF'
 };
